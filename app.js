f3f3d649e6956fb3eddc2b1b6037c6fcf3403828
Eduardo Moreira <110752961+EduDevCode@users.noreply.github.com>
2023-06-17 01:04:15 -0300

    add files

---
+++ app.js
@@ -0,0 +1,329 @@
+function calcular1()
+{
+    let resposta = document.getElementById("result").innerHTML = 'a) 6 de junho de 1944';
+
+    if(resposta === 'a) 6 de junho de 1944')
+    {
+        document.getElementById("botao").value = 'A RESPOSTA É:';
+    }
+
+}
+
+function calcular2()
+{
+    let resposta = document.getElementById("result").innerHTML = 'a) 7 de dezembro de 1941 ';
+
+    if(resposta === 'a) 7 de dezembro de 1941 ')
+    {
+        document.getElementById("botao").value = 'A RESPOSTA É:';
+    }
+
+}
+
+function calcular3()
+{
+    let resposta = document.getElementById("result").innerHTML = 'a) 6 de agosto de 1945';
+
+    if(resposta === 'a) 6 de agosto de 1945')
+    {
+        document.getElementById("botao").value = 'A RESPOSTA É:';
+    }
+
+}
+
+function calcular4()
+{
+    let resposta = document.getElementById("result").innerHTML = 'a) 11 de novembro de 1918';
+
+    if(resposta === 'a) 11 de novembro de 1918')
+    {
+        document.getElementById("botao").value = 'A RESPOSTA É:';
+    }
+
+}
+
+function calcular5()
+{
+    let resposta = document.getElementById("result").innerHTML = 'a) 2 de setembro de 1945';
+
+    if(resposta === 'a) 2 de setembro de 1945')
+    {
+        document.getElementById("botao").value = 'A RESPOSTA É:';
+    }
+
+}
+
+function calcular6()
+{
+    let resposta = document.getElementById("result").innerHTML = 'a) A população questionou o posicionamento contraditório de Vargas na Guerra';
+
+    if(resposta === 'a) A população questionou o posicionamento contraditório de Vargas na Guerra')
+    {
+        document.getElementById("botao").value = 'A RESPOSTA É:';
+    }
+
+}
+
+function calcular7()
+{
+    let resposta = document.getElementById("result").innerHTML = 'a) Invasão da Polônia';
+
+    if(resposta === 'a) Invasão da Polônia')
+    {
+        document.getElementById("botao").value = 'A RESPOSTA É:';
+    }
+
+}
+
+function calcular8()
+{
+    let resposta = document.getElementById("result").innerHTML = 'b) Blitzkrieg';
+
+    if(resposta === 'b) Blitzkrieg')
+    {
+        document.getElementById("botao").value = 'A RESPOSTA É:';
+    }
+
+}
+
+function calcular9()
+{
+    let resposta = document.getElementById("result").innerHTML = 'a) Joseph Stalin';
+
+    if(resposta === 'a) Joseph Stalin')
+    {
+        document.getElementById("botao").value = 'A RESPOSTA É:';
+    }
+
+}
+
+function calcular10()
+{
+    let resposta = document.getElementById("result").innerHTML = 'd) Japão';
+
+    if(resposta === 'd) Japão')
+    {
+        document.getElementById("botao").value = 'A RESPOSTA É:';
+    }
+
+}
+
+function calcular11()
+{
+    let resposta = document.getElementById("result").innerHTML = 'a) "We Shall Fight on the Beaches"';
+
+    if(resposta === 'a) "We Shall Fight on the Beaches"')
+    {
+        document.getElementById("botao").value = 'A RESPOSTA É:';
+    }
+
+}
+
+function calcular12()
+{
+    let resposta = document.getElementById("result").innerHTML = 'a) 6 de junho de 1944';
+
+    if(resposta === 'a) 6 de junho de 1944')
+    {
+        document.getElementById("botao").value = 'A RESPOSTA É:';
+    }
+
+}
+
+function calcular13()
+{
+    let resposta = document.getElementById("result").innerHTML = 'b)Porque ele impôs sanções que geraram um sentimento de revolta nos países do Eixo.';
+
+    if(resposta === 'b)Porque ele impôs sanções que geraram um sentimento de revolta nos países do Eixo.')
+    {
+        document.getElementById("botao").value = 'A RESPOSTA É:';
+    }
+
+}
+
+function calcular14()
+{
+    let resposta = document.getElementById("result").innerHTML = 'c)Crise de 1929 e consequências da Primeira Guerra.';
+
+    if(resposta === 'c)Crise de 1929 e consequências da Primeira Guerra.')
+    {
+        document.getElementById("botao").value = 'A RESPOSTA É:';
+    }
+
+}
+
+function calcular15()
+{
+    let resposta = document.getElementById("result").innerHTML = 'a)Política do Apaziguamento';
+
+    if(resposta === 'a)Política do Apaziguamento')
+    {
+        document.getElementById("botao").value = 'A RESPOSTA É:';
+    }
+
+}
+
+function calcular16()
+{
+    let resposta = document.getElementById("result").innerHTML = 'a)Política do expansionismo germânico.';
+
+    if(resposta === 'a)Política do expansionismo germânico.')
+    {
+        document.getElementById("botao").value = 'A RESPOSTA É:';
+    }
+
+}
+
+function calcular17()
+{
+    let resposta = document.getElementById("result").innerHTML = 'd)O interesse da Alemanha em anexar os Sudetos.';
+
+    if(resposta === 'd)O interesse da Alemanha em anexar os Sudetos.')
+    {
+        document.getElementById("botao").value = 'A RESPOSTA É:';
+    }
+
+}
+
+function calcular18()
+{
+    let resposta = document.getElementById("result").innerHTML = 'd) Franklin D. Roosevelt';
+
+    if(resposta === 'd) Franklin D. Roosevelt')
+    {
+        document.getElementById("botao").value = 'A RESPOSTA É:';
+    }
+
+}
+
+function calcular19()
+{
+    let resposta = document.getElementById("result").innerHTML = 'c) Winston Churchill';
+
+    if(resposta === 'c) Winston Churchill')
+    {
+        document.getElementById("botao").value = 'A RESPOSTA É:';
+    }
+
+}
+
+function calcular20()
+{
+    let resposta = document.getElementById("result").innerHTML = 'c) Dwight D. Eisenhower';
+
+    if(resposta === 'c) Dwight D. Eisenhower')
+    {
+        document.getElementById("botao").value = 'A RESPOSTA É:';
+    }
+
+}
+
+function calcular21()
+{
+    let resposta = document.getElementById("result").innerHTML = 'c) Hideki Tojo';
+
+    if(resposta === 'c) Hideki Tojo')
+    {
+        document.getElementById("botao").value = 'A RESPOSTA É:';
+    }
+
+}
+
+function calcular22()
+{
+    let resposta = document.getElementById("result").innerHTML = ' b) Benito Mussolini';
+
+    if(resposta === ' b) Benito Mussolini')
+    {
+        document.getElementById("botao").value = 'A RESPOSTA É:';
+    }
+
+}
+
+function calcular23()
+{
+    let resposta = document.getElementById("result").innerHTML = 'a) Projeto Manhattan';
+
+    if(resposta === 'a) Projeto Manhattan')
+    {
+        document.getElementById("botao").value = 'A RESPOSTA É:';
+    }
+
+}
+
+function calcular24()
+{
+    let resposta = document.getElementById("result").innerHTML = 'a) França';
+
+    if(resposta === 'a) França')
+    {
+        document.getElementById("botao").value = 'A RESPOSTA É:';
+    }
+
+}
+
+function calcular25()
+{
+    let resposta = document.getElementById("result").innerHTML = 'd) Nazismo';
+
+    if(resposta === 'd) Nazismo')
+    {
+        document.getElementById("botao").value = 'A RESPOSTA É:';
+    }
+
+}
+
+function calcular26()
+{
+    let resposta = document.getElementById("result").innerHTML = 'a) Operação Barbarossa';
+
+    if(resposta === 'a) Operação Barbarossa')
+    {
+        document.getElementById("botao").value = 'A RESPOSTA É:';
+    }
+
+}
+
+function calcular27()
+{
+    let resposta = document.getElementById("result").innerHTML = 'c) Batalha da Inglaterra';
+
+    if(resposta === 'c) Batalha da Inglaterra')
+    {
+        document.getElementById("botao").value = 'A RESPOSTA É:';
+    }
+
+}
+
+function calcular28()
+{
+    let resposta = document.getElementById("result").innerHTML = 'd) OMC (Organização Mundial do Comércio)';
+
+    if(resposta === 'd) OMC (Organização Mundial do Comércio)')
+    {
+        document.getElementById("botao").value = 'A RESPOSTA É:';
+    }
+
+}
+
+function calcular29()
+{
+    let resposta = document.getElementById("result").innerHTML = 'a) 60 milhões';
+
+    if(resposta === 'a) 60 milhões')
+    {
+        document.getElementById("botao").value = 'A RESPOSTA É:';
+    }
+
+}
+
+function calcular30()
+{
+    let resposta = document.getElementById("result").innerHTML = 'b) França, Reino Unido e União Soviética';
+
+    if(resposta === 'b) França, Reino Unido e União Soviética')
+    {
+        document.getElementById("botao").value = 'A RESPOSTA É:';
+    }
+
+}
